cd64dfe07d1bce5040126838ed5f479c11e6bb23
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-11-09 13:50:31 +0100

    tweaks

--- packages/async-ssr/src/use-loader.ts
+++ packages/async-ssr/src/use-loader.ts
@@ -1,6 +1,6 @@
-import { useContext, useEffect, useRef, useState } from "react";
-import useSSR from "use-ssr";
-import { AsyncResult, getAsyncContext } from "./context";
+import { useContext, useEffect, useState } from 'react'
+import useSSR from 'use-ssr'
+import { AsyncResult, getAsyncContext } from './context'
 
 export interface LoaderResult<T> {
     data?: T;
@@ -24,7 +24,7 @@ export interface LoaderOptions {
      */
     refreshClient?: boolean
     /**
-     * Sets the cache time in ms. 
+     * Sets the cache time in ms.
      * A cache time at zero or below, disables it
      * @type {number}
      * @memberof LoaderOptions
@@ -40,31 +40,26 @@ export interface LoaderOptions {
 }
 
 export function useLoader<T>(key: string, init: () => Promise<T>, options: LoaderOptions = {}, deps?: any[]): LoaderResult<T> {
-
-    const { ssr = true, refreshClient = false, ttl = 0, enabled = true } = options;
-    const { isServer } = useSSR();
-    const currentKey = useRef<string | null>(null)
+    const { ssr = true, refreshClient = false, ttl = 0, enabled = true } = options
+    const { isServer } = useSSR()
 
     if (isServer && (!ssr || !enabled)) {
         return enabled ? { loading: true } : { loading: false }
     }
 
-    const ctx = useContext(getAsyncContext());
+    const ctx = useContext(getAsyncContext())
 
     if (isServer) {
-        ctx.add(key, init, ttl);
-        return ctx.get(key) as LoaderResult<T>;
-    } /*else if (!isServer && ssr && !refreshClient) {
-        return ctx.get(key);
-    }*/
+        ctx.add(key, init, ttl)
+        return ctx.get(key) as LoaderResult<T>
+    }
 
     const [state, setState] = useState<AsyncResult<T>>(ctx.get(key) ?? {
-        loading: enabled,
+        loading: enabled
 
     })
 
     useEffect(() => {
-
         if (!enabled || (ssr && !refreshClient)) {
             return
         }
@@ -72,7 +67,7 @@ export function useLoader<T>(key: string, init: () => Promise<T>, options: Loade
         ctx.add(key, init, ttl).then(data => {
             setState({
                 loading: false,
-                data,
+                data
             })
         }, error => {
             setState({
@@ -80,52 +75,7 @@ export function useLoader<T>(key: string, init: () => Promise<T>, options: Loade
                 error
             })
         })
-    }, (deps ?? []).concat(enabled, refreshClient));
+    }, (deps ?? []).concat(enabled, refreshClient))
 
     return state
 }
-
-
-// export function useLoader2<T>(key: string, init: () => Promise<T>, options: LoaderOptions = {}, deps?: any[]): LoaderResult<T> {
-
-//     const { ssr = true, refreshClient = false, ttl = 0, enabled = true } = options;
-//     const { isServer } = useSSR();
-
-//     if (isServer && (!ssr || !enabled)) {
-//         return enabled ? { loading: true } : { loading: false }
-//     }
-
-//     const ctx = useContext(getAsyncContext());
-
-//     if (isServer) {
-//         ctx.add(key, init, ttl);
-//         return ctx.get(key);
-//     } /*else if (!isServer && ssr && !refreshClient) {
-//         return ctx.get(key);
-//     }*/
-
-//     const [state, setState] = useState<AsyncResult<T>>(ctx.get(key) ?? {
-//         loading: enabled
-//     })
-
-//     useEffect(() => {
-
-//         if (!enabled) {
-//             return
-//         }
-
-//         ctx.add(key, init, ttl).then(data => {
-//             setState({
-//                 loading: false,
-//                 data,
-//             })
-//         }, error => {
-//             setState({
-//                 loading: false,
-//                 error
-//             })
-//         })
-//     }, (deps ?? []).concat(enabled));
-
-//     return state
-// }

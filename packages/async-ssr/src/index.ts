6961f08a7baec16015993e4d398dd85bbe3166b5
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-12-02 16:48:55 +0100

    export isCancel util fn

--- packages/async-ssr/src/index.ts
+++ packages/async-ssr/src/index.ts
@@ -2,4 +2,4 @@ export * from './use-loader';
 export * from './use-fetch';
 export * from './cache';
 export { AsyncManager, AsyncManagerProps } from './client';
-export { useIsomorphicLayoutEffect } from './util';
+export { useIsomorphicLayoutEffect, isCancel } from './util';
